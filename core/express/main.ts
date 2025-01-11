65d94793b4df2c6ab4ae489e948ceffa7b6ccbc4
Hasan Orhan <hasorh96@gmail.com>
2025-01-11 15:29:14 +0300

    message updates & status fixes for render route

--- core/express/main.ts
+++ core/express/main.ts
@@ -18,46 +18,57 @@ app.get("/", async (req, res) => {
 
 // route: render
 app.post("/templates/:template/render", async (req, res, next) => {
-    // target template
-    const template = templates[req.params.template as keyof typeof templates];
+    // get slug from request
+    const target = req.params.template as keyof typeof templates;
 
     // when there no template for slug
-    if (template == undefined) {
-        res.json({
+    if (!Object.hasOwn(templates, target)) {
+        res.status(404).json({
             message: "NOT_FOUND",
-        }).status(404);
+        });
 
         return next();
     }
 
-    // get thw props from req
+    // target template
+    const template = templates[target];
+
+    // get the props from req
     const props = req.body as {};
 
     // zod validation
     if (template.schema) {
         try {
-            template.schema.parse(req.body);
-            next();
+            template.schema.parse(props);
+
+            //
         } catch (err) {
-            if (err instanceof ZodError) {
-                const errors: { [key: string]: string[] } = {};
-
-                err.issues.map((e) => {
-                    e.path.map((p) => {
-                        if (!errors[p]) {
-                            errors[p] = [];
-                        }
-                        errors[p].push(e.message);
-                    });
+            // if its another kind of error (?)
+            if (!(err instanceof ZodError)) {
+                res.status(500).json({
+                    message: "UNKNOWN_ERROR",
                 });
 
-                res.status(422).json({
-                    message: "The given data was invalid.",
-                    errors: errors,
-                });
-
-                return;
+                return next();
             }
+
+            // bag for error
+            const errors: { [key: string]: string[] } = {};
+
+            // map the issues
+            err.issues.map((issue) =>
+                issue.path.map((path) => {
+                    errors[path] = errors[path] || [];
+                    errors[path].push(issue.message);
+                })
+            );
+
+            res.status(422).json({
+                message: "INVALID_PAYLOAD",
+                errors: errors,
+            });
+
+            return next();
         }
     }
 
@@ -65,7 +76,7 @@ app.post("/templates/:template/render", async (req, res, next) => {
     const component = createElement(template.component, props);
 
     // render
-    res.json({
+    res.status(200).json({
         message: "SUCCESS",
         body: {
             html: await render(component),
@@ -73,7 +84,7 @@ app.post("/templates/:template/render", async (req, res, next) => {
                 plainText: true,
             }),
         },
-    }).status(200);
+    });
 
     return next();
 });
